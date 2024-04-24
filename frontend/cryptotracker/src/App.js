c13889f6814e4a695bb985e15f77b8eb8e1e5a15
Alan Arias <alan.arias11@gmail.com>
2024-04-24 19:03:15 -0400

    Added Google Analytics

--- frontend/cryptotracker/src/App.js
+++ frontend/cryptotracker/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Buffer } from 'buffer'; // Import Buffer from the buffer package
+import { Buffer } from 'buffer';
+import { Routes, Route, BrowserRouter as Router, useLocation } from 'react-router-dom';
+import { analytics } from './firebase';
+import { logEvent } from "firebase/analytics";
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
@@ -12,19 +15,16 @@ import WatchList from './pages/WatchList';
 import Portfolio from './pages/Portfolio';
 import CryptoPage from './pages/CryptoPage';
 import Exchanges from './pages/Exchanges';
-import { useTheme } from './components/ThemeContext/ThemeContext';
-import Footer from './components/Footer/Footer';
 import CommunityPage from './pages/CommunityPage';
-import { getAuth, onAuthStateChanged } from "firebase/auth";
 import CommunityProfile from './pages/CommunityProfile';
 import Account from './pages/Account';
-import useScrollToTop from './components/ScrollToTop/useScrollToTop';
+import CommunityUserProfile from './pages/CommunityUserProfile';
+import Footer from './components/Footer/Footer';
+import { useTheme } from './components/ThemeContext/ThemeContext';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import CoinbaseWalletSDK from '@coinbase/wallet-sdk';
 import Web3 from 'web3';
-import CommunityUserProfile from './pages/CommunityUserProfile';
 import '@typehaus/metropolis';
-import { analytics } from './firebase';
-import { logEvent } from "firebase/analytics";
 
 window.Buffer = Buffer; // Assign Buffer to window to make it globally available
 
@@ -38,9 +38,21 @@ const coinbaseWallet = new CoinbaseWalletSDK({
   appLogoUrl: APP_LOGO_URL,
   supportedChainIds: APP_SUPPORTED_CHAIN_IDS,
 });
-
 const ethereum = coinbaseWallet.makeWeb3Provider();
 export const web3 = new Web3(ethereum);
+
+function usePageTracking() {
+  const location = useLocation();
+  useEffect(() => {
+    // Log the current page
+    logEvent(analytics, 'page_view', { page_path: location.pathname });
+  }, [location]);
+}
+
+function PageTracker() {
+  usePageTracking();
+  return null;
+}
 
 function App() {
   const [user, setUser] = useState(null);
@@ -61,35 +73,36 @@ function App() {
     };
   }, [auth]);
 
-  // Function to disconnect from Coinbase Wallet
   const disconnectCoinbaseWallet = () => {
     coinbaseWallet.disconnect();
     console.log('Disconnected from Coinbase Wallet');
-    // Perform any additional cleanup or state updates as needed
   };
 
   return (
-    <div className={`App ${
-      theme === "dark" ? "bg-[#031021] text-primary-50" : "bg-[#F5F9FE] text-primary-800"
-    }`}>
-      <Header setUser={setUser} user={user} />
-      <Navbar setUser={setUser} user={user} />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/heatmap' element={<HeatMap />} />
-        <Route path='/watchlist' element={<WatchList />} />
-        <Route path='/portfolio' element={<Portfolio />} />
-        <Route path='/cryptopage/:symbol' element={<CryptoPage user={user} />} />
-        <Route path='/exchanges' element={<Exchanges />} />
-        <Route path='/community' element={<CommunityPage user={user} />} />
-        <Route path='/community/profile/:displayname' element={<CommunityProfile user={user} />} />
-        <Route path='/account' element={<Account user={user} />} />
-        <Route path="/community/userprofile/:id" element={<CommunityUserProfile />} />
-      </Routes>
-      <Footer />
-    </div>
+    <Router>
+      <PageTracker />
+      <div className={`App ${
+        theme === "dark" ? "bg-[#031021] text-primary-50" : "bg-[#F5F9FE] text-primary-800"
+      }`}>
+        <Header setUser={setUser} user={user} />
+        <Navbar setUser={setUser} user={user} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/heatmap' element={<HeatMap />} />
+          <Route path='/watchlist' element={<WatchList />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/cryptopage/:symbol' element={<CryptoPage user={user} />} />
+          <Route path='/exchanges' element={<Exchanges />} />
+          <Route path='/community' element={<CommunityPage user={user} />} />
+          <Route path='/community/profile/:displayname' element={<CommunityProfile user={user} />} />
+          <Route path='/account' element={<Account user={user} />} />
+          <Route path="/community/userprofile/:id" element={<CommunityUserProfile />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
