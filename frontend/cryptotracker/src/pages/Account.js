1f4be346dc32cf864cede6e6baacdcb3b460a2aa
Alan Arias <alan.arias11@gmail.com>
2024-04-25 20:12:21 -0400

    Updated Account Page Layout and Responsiveness with Dark and Light Mode

--- frontend/cryptotracker/src/pages/Account.js
+++ frontend/cryptotracker/src/pages/Account.js
@@ -69,64 +69,65 @@ function Account({ user }) {
   console.log(user);
 
   return (
-    <div className="flex">
-      {/* Sidebar */}
-      <div className="w-64 h-screen bg-gray-800 text-white">
-        <div className="p-5">
-          <h2 className="font-bold">Account Settings</h2>
-          <nav className="mt-10">
-            <a href="#" className="block py-2.5 px-4 rounded hover:bg-gray-700">
-              Profile
-            </a>
-            <a href="#" className="block py-2.5 px-4 rounded hover:bg-gray-700">
-              Security
-            </a>
-            <a href="#" className="block py-2.5 px-4 rounded hover:bg-gray-700">
-              Notifications
-            </a>
-          </nav>
-        </div>
-      </div>
-
-      {/* Main Content */}
-      <div className="flex-1 p-10">
-        <h1 className="text-2xl font-semibold">Edit Profile</h1>
-        <div className="mt-8 max-w-md">
-          <div className="flex items-center">
-            <span className="inline-block h-24 w-24 rounded-full overflow-hidden bg-gray-100">
-              {profilePic ? (
-                <img className="w-full h-full object-cover" src={profilePic} alt="Profile" />
-              ) : (
-                <CameraIcon className="h-full w-full text-gray-300" />
-              )}
-            </span>
-            <input
-              type="file"
-              onChange={handleProfilePicChange}
-              className="ml-5 bg-slate-500 p-2 rounded cursor-pointer"
-            />
-          </div>
-          <div className="mt-4">
-            <label className="block">Display Name</label>
-            <input
-              type="text"
-              value={displayName}
-              onChange={handleDisplayNameChange}
-              className="mt-1 block w-full px-3 py-2 bg-slate-500 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-            />
-          </div>
-          <button
-            onClick={saveChanges}
-            className={`mt-4 px-4 py-2 ${
-              uploading ? "bg-gray-500" : "bg-blue-500"
-            } text-white rounded hover:bg-blue-600`}
-            disabled={uploading}
-          >
-            {uploading ? "Saving..." : "Save Changes"}
-          </button>
-        </div>
+    <div className={`flex flex-col md:flex-row ${
+      theme === "dark" ? "bg-[#031021] text-primary-50" : "bg-[#F5F9FE] text-primary-800"
+    }`}>
+    {/* Sidebar */}
+    <div className={`w-full md:w-64 h-auto hidden md:flex md: md:h-screen ${
+            theme == "dark"
+              ? "border-zinc-700 bg-gradient-to-l from-[#07172b]"
+              : "bg-primary-50 shadow-primary-100 border-primary-200"
+          }`}>
+      <div className="p-5">
+        <h2 className="font-bold">Account Settings</h2>
+        <nav className="mt-10">
+          <Link to="#" className="block py-2.5 px-4 rounded hover:bg-gray-700">Profile</Link>
+          
+        </nav>
       </div>
     </div>
+
+    {/* Main Content */}
+    <div className="flex-1 p-10">
+      <h1 className="text-2xl font-semibold">Edit Profile</h1>
+      <div className="mt-8 max-w-md">
+        <div className="flex flex-col gap-2 md:flex items-center">
+          <span className="h-24 w-24 rounded-full overflow-hidden bg-gray-100">
+            {profilePic ? (
+              <img src={profilePic} alt="Profile" className="w-full h-full object-cover" />
+            ) : (
+              <CameraIcon className="h-full w-full text-gray-300" />
+            )}
+          </span>
+          <input
+            type="file"
+            onChange={handleProfilePicChange}
+            className={`search-input ml-5 p-2 ${
+              theme === "dark" ? "bg-[#031021] border border-primary-300 text-primary-200" : "bg-white"
+            }`}
+          />
+        </div>
+        <div className="mt-4">
+          <label className="block">Display Name</label>
+          <input
+            type="text"
+            value={displayName}
+            onChange={handleDisplayNameChange}
+            className={`search-input w-full p-2 ${
+              theme === "dark" ? "bg-[#031021] border border-primary-300 text-primary-200" : ""
+            }`}
+          />
+        </div>
+        <button
+          onClick={saveChanges}
+          className={`mt-4 px-4 py-2 ${uploading ? "bg-gray-500" : "bg-blue-500"} text-white rounded hover:bg-blue-600`}
+          disabled={uploading}
+        >
+          {uploading ? "Saving..." : "Save Changes"}
+        </button>
+      </div>
+    </div>
+  </div>
   );
 }
 
