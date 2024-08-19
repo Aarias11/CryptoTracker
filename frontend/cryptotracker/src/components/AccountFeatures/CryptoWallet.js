05849de38fad008cbb0777775bd0ffdc48e7ae69
Alan Arias <alanarias@Alans-Air.lan>
2024-08-19 18:14:33 -0400

    Updated the padding for Account page

--- frontend/cryptotracker/src/components/AccountFeatures/CryptoWallet.js
+++ frontend/cryptotracker/src/components/AccountFeatures/CryptoWallet.js
@@ -2,12 +2,11 @@ import React from "react";
 
 const CryptoWallet = () => {
   return (
-    <div className="w-full h-auto md:h-[300px]">
-      <h2 className="headline-semibold-24 mt-20">Crypto Wallet</h2>
-      <div className="w-full h-full flex justify-center">
+    <div className="w-full h-auto md:h-[400px]">
+      <div className="w-full h-full flex justify-center items-center">
         <div className="w-[70%] mt-5">
         <h3 className="headline-semibold-36">Connect your crypto wallet</h3>
-      <p className="mt-4 w-auto lg:w-[570px] ">It looks like you don't have a wallet connect yet. Add an external crypto wallet to your account to easily manage your assets, track investments, and streamline transactions. Connect your wallet now to enjoy a seamless crypto experience!</p>
+      <p className="mt-4 w-auto lg:w-[570px] ">It looks like you don't have a wallet connected yet. Add an external crypto wallet to your account to easily manage your assets, track investments, and streamline transactions. Connect your wallet now to enjoy a seamless crypto experience!</p>
       {/* Add your crypto wallet management form here */}
       <div className="w-[70%] flex justify-center">
       <button className="w-[130px] h-[40px] bg-information-700 label-14 rounded-lg transition duration-300 ease-in-out shadow-lg shadow-information-800 mt-5">Connect Wallet</button>
--- frontend/cryptotracker/src/pages/Account.js
+++ frontend/cryptotracker/src/pages/Account.js
@@ -4,25 +4,25 @@ import ThemeContext from "../components/ThemeContext/ThemeContext";
 import { getAuth, updateProfile } from "firebase/auth";
 import { db, doc, setDoc } from '../firebase';
 import { ref, getStorage, uploadBytes, getDownloadURL } from "firebase/storage";
-import AccountSecurity from "../components/AccountFeatures/AccountSecurity";  // Import the AccountSecurity component
-import CryptoWallet from "../components/AccountFeatures/CryptoWallet";        // Import the CryptoWallet component
+import AccountSecurity from "../components/AccountFeatures/AccountSecurity";
+import CryptoWallet from "../components/AccountFeatures/CryptoWallet";
 import LoadingComponent from "../components/LoadingComponent";
 
 function Account({ user }) {
   const [displayName, setDisplayName] = useState("");
   const [profilePic, setProfilePic] = useState("");
-  const [uploading, setUploading] = useState(false); // Track upload status
+  const [uploading, setUploading] = useState(false);
   const { theme } = useContext(ThemeContext);
   const auth = getAuth();
-  const storage = getStorage(); // Initialize Firebase Storage
-  const [selectedTab, setSelectedTab] = useState("profile"); // State for managing selected tab
+  const storage = getStorage();
+  const [selectedTab, setSelectedTab] = useState("profile");
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (user) {
       setDisplayName(user.displayName || "");
       setProfilePic(user.photoURL || "");
-      setLoading(false); // Set loading to false once user data is loaded
+      setLoading(false);
     }
   }, [user]);
 
@@ -50,18 +50,16 @@ function Account({ user }) {
 
     setUploading(true);
     try {
-      // Update the auth profile
       await updateProfile(auth.currentUser, {
         displayName: displayName,
         photoURL: profilePic,
       });
 
-      // Save additional information to Firestore
-      const userRef = doc(db, "users", user.uid); // Reference to the user's document in Firestore
+      const userRef = doc(db, "users", user.uid);
       await setDoc(userRef, {
-        displayName: displayName, // Save the display name
-        photoURL: profilePic, // Save the profile picture URL
-      }, { merge: true }); // Merge with existing document to prevent overwriting other fields
+        displayName: displayName,
+        photoURL: profilePic,
+      }, { merge: true });
 
       alert("Profile updated successfully");
     } catch (error) {
@@ -74,94 +72,105 @@ function Account({ user }) {
     return <LoadingComponent theme={theme} />;
   }
 
-  // Render different components based on the selected tab
   const renderContent = () => {
     switch (selectedTab) {
       case "profile":
         return (
-          <div className="mt-8 max-w-md">
-            <div className="flex flex-col gap-2 md:flex items-center">
-              <span className="h-24 w-24 rounded-full overflow-hidden bg-gray-100">
-                {profilePic ? (
-                  <img src={profilePic} alt="Profile" className="w-full h-full object-cover" />
-                ) : (
-                  <CameraIcon className="h-full w-full text-gray-300" />
-                )}
-              </span>
-              <input
-                type="file"
-                onChange={handleProfilePicChange}
-                id="profilePicInput"
-                className="hidden"
-              />
-              <label
-                htmlFor="profilePicInput"
-                className={`w-[100px] h-[40px] label-14 rounded-lg transition duration-300 ease-in-out shadow-lg shadow-primary-800 ${
-                  theme === "dark"
-                    ? "button-primary-medium-dark text-primary-50"
-                    : "button-primary-medium-light text-primary-50"
-                }`}
+          <div>
+            <h2 className="headline-semibold-28 mb-6">Profile</h2>
+            <div className="mt-8 max-w-md">
+              <div className="flex flex-col gap-2 md:flex items-center">
+                <span className="h-24 w-24 rounded-full overflow-hidden bg-gray-100">
+                  {profilePic ? (
+                    <img src={profilePic} alt="Profile" className="w-full h-full object-cover" />
+                  ) : (
+                    <CameraIcon className="h-full w-full text-gray-300" />
+                  )}
+                </span>
+                <input
+                  type="file"
+                  onChange={handleProfilePicChange}
+                  id="profilePicInput"
+                  className="hidden"
+                />
+                <label
+                  htmlFor="profilePicInput"
+                  className={`w-[100px] h-[40px] label-14 rounded-lg transition duration-300 ease-in-out shadow-lg shadow-primary-800 ${
+                    theme === "dark"
+                      ? "button-primary-medium-dark text-primary-50"
+                      : "button-primary-medium-light text-primary-50"
+                  }`}
+                >
+                  Browse
+                </label>
+              </div>
+              <div className="mt-4">
+                <label className="block mb-2">Display Name</label>
+                <input
+                  type="text"
+                  value={displayName}
+                  onChange={handleDisplayNameChange}
+                  className={`search-input w-full p-2 ${
+                    theme === "dark" ? "bg-[#031021] border border-primary-300 text-primary-200" : ""
+                  }`}
+                />
+              </div>
+              <button
+                onClick={saveChanges}
+                className={`mt-4 px-4 py-2 w-[150px] h-[40px] label-14 rounded-lg transition duration-300 ease-in-out shadow-lg shadow-information-800 bg-information-700 ${uploading ? "text-primary-50" : " text-primary-50"} text-white rounded hover:bg-information-600`}
+                disabled={uploading}
               >
-                Browse
-              </label>
+                {uploading ? "Saving..." : "Save Changes"}
+              </button>
             </div>
-            <div className="mt-4">
-              <label className="block mb-2">Display Name</label>
-              <input
-                type="text"
-                value={displayName}
-                onChange={handleDisplayNameChange}
-                className={`search-input w-full p-2 ${
-                  theme === "dark" ? "bg-[#031021] border border-primary-300 text-primary-200" : ""
-                }`}
-              />
-            </div>
-            <button
-              onClick={saveChanges}
-              className={`mt-4 px-4 py-2 w-[150px] h-[40px] label-14 rounded-lg transition duration-300 ease-in-out shadow-lg shadow-information-800 bg-information-700 ${uploading ? "text-primary-50" : " text-primary-50"} text-white rounded hover:bg-information-600`}
-              disabled={uploading}
-            >
-              {uploading ? "Saving..." : "Save Changes"}
-            </button>
           </div>
         );
       case "security":
-        return <AccountSecurity theme={theme} />;
+        return (
+          <div>
+            <h2 className="headline-semibold-28 mb-6">Account Security</h2>
+            <AccountSecurity theme={theme} />
+          </div>
+        );
       case "wallet":
-        return <CryptoWallet />;
+        return (
+          <div>
+            <h2 className="headline-semibold-28 mb-6">Crypto Wallet</h2>
+            <CryptoWallet />
+          </div>
+        );
       default:
         return null;
     }
   };
 
   return (
-    <div className={`flex flex-col md:flex-row ${
+    <div className={`flex flex-col md:flex-row  ${
       theme === "dark" ? "bg-[#031021] text-primary-50" : "bg-[#F5F9FE] text-primary-800"
     }`}>
-      {/* Sidebar */}
-      <div className={`w-full md:w-64 h-auto hidden md:flex md:h-screen ${
+      <div className={`w-full md:w-72 h-auto hidden md:flex md:h-screen px-4 ${
         theme === "dark"
           ? "border-zinc-700 bg-gradient-to-l from-[#07172b]"
           : "bg-primary-50 shadow-primary-100 border-primary-200"
       }`}>
-        <div className="p-10 ">
+        <div className="p-10">
           <h2 className="headline-semibold-28 ml-2">Account</h2>
           <nav className="mt-10 body-16">
             <button
               onClick={() => setSelectedTab("profile")}
-              className={`block py-2.5 px-4 rounded hover:bg-gray-700 ${selectedTab === "profile" ? "rounded-none border-l-2 border-blue-500 transition duration-300 ease-in-out " : ""}`}
+              className={`block py-2.5 px-4 rounded hover:bg-primary-700 ${selectedTab === "profile" ? "rounded-none border-l-2 border-blue-500 transition duration-300 ease-in-out " : ""}`}
             >
               Profile
             </button>
             <button
               onClick={() => setSelectedTab("security")}
-              className={`block py-2.5 px-4 rounded hover:bg-gray-700 ${selectedTab === "security" ? "rounded-none border-l-2 border-blue-500 transition duration-300 ease-in-out " : ""}`}
+              className={`block py-2.5 px-4 rounded hover:bg-primary-700 ${selectedTab === "security" ? "rounded-none border-l-2 border-blue-500 transition duration-300 ease-in-out " : ""}`}
             >
               Account Security
             </button>
             <button
               onClick={() => setSelectedTab("wallet")}
-              className={`block py-2.5 px-4 rounded hover:bg-gray-700 ${selectedTab === "wallet" ? "rounded-none border-l-2 border-blue-500 transition duration-700 ease-in-out " : ""}`}
+              className={`block py-2.5 px-4 rounded hover:bg-primary-700 ${selectedTab === "wallet" ? "rounded-none border-l-2 border-blue-500 transition duration-700 ease-in-out " : ""}`}
             >
               Crypto Wallet
             </button>
@@ -169,7 +178,6 @@ function Account({ user }) {
         </div>
       </div>
 
-      {/* Main Content */}
       <div className="flex-1 p-10">
         {renderContent()}
       </div>
